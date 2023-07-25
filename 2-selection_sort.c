caece13f15df8c25cf60d76915cfb965801b9479
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-07-25 09:01:35 -0700

    main files and selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * selection_sort - A function that sorts an array of numbers
+ * using the selection sort algorithm
+ *
+ * @array: The array to sort
+ * @size: the size of the array
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t temp, min, j, i = 0;
+
+	if (size == 0 || !array)
+		return;
+
+	while (i < (size - 1))
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+		print_array(array, size);
+		i++;
+	}
+}
