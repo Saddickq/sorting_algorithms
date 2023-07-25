b3c849e7c76a659fdab909c5a5c97add4de66cf4
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-07-25 08:25:51 -0700

    header file and print array function

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **head, listint_t *node1, listint_t *node2);
+void selection_sort(int *array, size_t size);
+
+#endif
