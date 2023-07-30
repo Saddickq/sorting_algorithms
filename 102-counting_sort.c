738ada99e3a08529cf350720b426a76cf6a0c87b
10thcode <attabadukwabena@gmail.com>
2023-07-30 00:54:09 +0000

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,7 +11,7 @@ void counting_sort(int *array, size_t size)
 {
 	int i, max = array[0], *counts, *sorted = malloc(sizeof(int) * size);
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
 
 	for (i = 1; (unsigned long int) i < size; i++)
